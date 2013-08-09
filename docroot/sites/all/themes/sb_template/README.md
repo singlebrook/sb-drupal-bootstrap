42032d4ad7deeb3483768281a38ace4b64b518b5
Aaron Froehlich <aaron@singlebrook.com>
2013-08-09 13:31:26 -0400

    Adds modified sb_theme_template

---
+++ docroot/sites/all/themes/sb_template/README.md
@@ -0,0 +1,7 @@
+# SB Theme Template
+
+## Setup
+See main readme for information about setting up this theme. 
+
+## SCSS
+The main style.scss file is a manifest for the compiled style.css file. Make sure to add new .scss files to the manifest.
---
+++ docroot/sites/all/themes/sb_template/template.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Implements hook_preprocess_hook()
+ *
+ * Adds custom markup to footer. 
+ * Adds modernizr.js
+ */
+function sb_template_preprocess_page(&$variables) {
+  // Footer content
+  $variables['page']['footer'][] = array('#markup' => '<div class="copyright">&copy; ' . date('Y') . ' Client Name </div>');
+  
+  drupal_add_js($variables['directory'] . '/js/vendor/modernizr-2.6.2.min.js');
+}
