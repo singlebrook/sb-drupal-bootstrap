179621a805ff39c99d4af1e7d9935d90eb158b6e
Aaron Froehlich <aaron@singlebrook.com>
2013-08-13 10:51:58 -0400

    Adds replacement token to footer

--- docroot/sites/all/themes/sb_template/template.php
+++ docroot/sites/all/themes/sb_template/template.php
@@ -6,9 +6,9 @@
  * Adds custom markup to footer. 
  * Adds modernizr.js
  */
-function sb_template_preprocess_page(&$variables) {
+function une_theme_preprocess_page(&$variables) {
   // Footer content
-  $variables['page']['footer'][] = array('#markup' => '<div class="copyright">&copy; ' . date('Y') . ' Client Name </div>');
+  $variables['page']['footer'][] = array('#markup' => '<div class="copyright">&copy; ' . date('Y') . ' SB_CLIENT </div>');
   
   drupal_add_js($variables['directory'] . '/js/vendor/modernizr-2.6.2.min.js');
 }
