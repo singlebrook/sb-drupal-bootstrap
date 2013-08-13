05767b690b6b018eaaf5efa070db9e5f58207596
Jeff Amaral <jeff@freethoughtdesign.com>
2013-08-13 13:03:27 -0400

    Fixes hardcoded theme references

--- docroot/sites/all/themes/sb_template/template.php
+++ docroot/sites/all/themes/sb_template/template.php
@@ -3,12 +3,12 @@
 /**
  * Implements hook_preprocess_hook()
  *
- * Adds custom markup to footer. 
+ * Adds custom markup to footer.
  * Adds modernizr.js
  */
-function une_theme_preprocess_page(&$variables) {
+function sb_template_preprocess_page(&$variables) {
   // Footer content
   $variables['page']['footer'][] = array('#markup' => '<div class="copyright">&copy; ' . date('Y') . ' SB_CLIENT </div>');
-  
+
   drupal_add_js($variables['directory'] . '/js/vendor/modernizr-2.6.2.min.js');
 }
