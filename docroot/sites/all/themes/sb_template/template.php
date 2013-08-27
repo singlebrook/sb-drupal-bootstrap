e5bd61111c62fb12bce7d330f3ebc860965f1148
Aaron Froehlich <aaron@singlebrook.com>
2013-08-27 10:03:46 -0400

    Cleans up init script

--- docroot/sites/all/themes/sb_template/template.php
+++ docroot/sites/all/themes/sb_template/template.php
@@ -8,7 +8,7 @@
  */
 function sb_template_preprocess_page(&$variables) {
   // Footer content
-  $variables['page']['footer'][] = array('#markup' => '<div class="copyright">&copy; ' . date('Y') . ' SB_CLIENT </div>');
+  $variables['page']['footer'][] = array('#markup' => '<div class="copyright">&copy; ' . date('Y') . ' CLIENT_NAME </div>');
 
   drupal_add_js($variables['directory'] . '/js/vendor/modernizr-2.6.2.min.js');
 }
